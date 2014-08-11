276f30c5b0f2a602d3ab0547451c87204f6bb4ca
Matt Tytel <mtytel@leapmotion.com>
2014-08-11 15:13:30 -0700

    FOV in editor completed!

--- Assets/LeapMotion/Editor/HandControllerEditor.cs
+++ Assets/LeapMotion/Editor/HandControllerEditor.cs
@@ -5,31 +5,74 @@ using System.Collections;
 [CustomEditor(typeof(HandController))]
 public class HandControllerEditor : Editor {
 
-  private const float BOX_HEIGHT = 0.45f;
+  private const float BOX_RADIUS = 0.45f;
   private const float BOX_WIDTH = 0.965f;
   private const float BOX_DEPTH = 0.6671f;
 
   public void OnSceneGUI() {
     HandController controller = (HandController)target;
     Vector3 origin = controller.transform.TransformPoint(Vector3.zero);
+
+    Vector3 local_top_left = new Vector3(-BOX_WIDTH, BOX_RADIUS, BOX_DEPTH);
     Vector3 top_left =
-        controller.transform.TransformPoint(new Vector3(-BOX_WIDTH, BOX_HEIGHT, BOX_DEPTH));
+        controller.transform.TransformPoint(BOX_RADIUS * local_top_left.normalized);
+
+    Vector3 local_top_right = new Vector3(BOX_WIDTH, BOX_RADIUS, BOX_DEPTH);
     Vector3 top_right =
-        controller.transform.TransformPoint(new Vector3(BOX_WIDTH, BOX_HEIGHT, BOX_DEPTH));
+        controller.transform.TransformPoint(BOX_RADIUS * local_top_right.normalized);
+
+    Vector3 local_bottom_left = new Vector3(-BOX_WIDTH, BOX_RADIUS, -BOX_DEPTH);
     Vector3 bottom_left =
-        controller.transform.TransformPoint(new Vector3(-BOX_WIDTH, BOX_HEIGHT, -BOX_DEPTH));
+        controller.transform.TransformPoint(BOX_RADIUS * local_bottom_left.normalized);
+
+    Vector3 local_bottom_right = new Vector3(BOX_WIDTH, BOX_RADIUS, -BOX_DEPTH);
     Vector3 bottom_right =
-        controller.transform.TransformPoint(new Vector3(BOX_WIDTH, BOX_HEIGHT, -BOX_DEPTH));
+        controller.transform.TransformPoint(BOX_RADIUS * local_bottom_right.normalized);
 
     Handles.DrawLine(origin, top_left);
     Handles.DrawLine(origin, top_right);
     Handles.DrawLine(origin, bottom_left);
     Handles.DrawLine(origin, bottom_right);
 
-    Handles.DrawLine(bottom_left, top_left);
-    Handles.DrawLine(top_left, top_right);
-    Handles.DrawLine(top_right, bottom_right);
-    Handles.DrawLine(bottom_right, bottom_left);
+    Vector3 top_normal = controller.transform.TransformDirection(
+        Vector3.Cross(local_top_left, local_top_right));
+    float top_angle = Vector3.Angle(local_top_left, local_top_right);
+    Handles.DrawWireArc(origin, top_normal, local_top_left,
+                        top_angle, controller.transform.localScale.x * BOX_RADIUS);
+
+    Vector3 left_normal = controller.transform.TransformDirection(
+        Vector3.Cross(local_bottom_left, local_top_left));
+    float left_angle = Vector3.Angle(local_bottom_left, local_top_left);
+    Handles.DrawWireArc(origin, left_normal, local_bottom_left,
+                        left_angle, controller.transform.localScale.x * BOX_RADIUS);
+
+    Vector3 bottom_normal = controller.transform.TransformDirection(
+        Vector3.Cross(local_bottom_left, local_bottom_right));
+    float bottom_angle = Vector3.Angle(local_bottom_left, local_bottom_right);
+    Handles.DrawWireArc(origin, bottom_normal, local_bottom_left,
+                        bottom_angle, controller.transform.localScale.x * BOX_RADIUS);
+
+    Vector3 right_normal = controller.transform.TransformDirection(
+        Vector3.Cross(local_bottom_right, local_top_right));
+    float right_angle = Vector3.Angle(local_bottom_right, local_top_right);
+    Handles.DrawWireArc(origin, right_normal, local_bottom_right,
+                        right_angle, controller.transform.localScale.x * BOX_RADIUS);
+
+    Vector3 local_left_face = Vector3.Lerp(local_top_left, local_bottom_left, 0.5f);
+    Vector3 local_right_face = Vector3.Lerp(local_top_right, local_bottom_right, 0.5f);
+
+    Vector3 across_normal = controller.transform.TransformDirection(-Vector3.forward);
+    float across_angle = Vector3.Angle(local_left_face, local_right_face);
+    Handles.DrawWireArc(origin, across_normal, local_left_face,
+                        across_angle, controller.transform.localScale.x * BOX_RADIUS);
+
+    Vector3 local_top_face = Vector3.Lerp(local_top_left, local_top_right, 0.5f);
+    Vector3 local_bottom_face = Vector3.Lerp(local_bottom_left, local_bottom_right, 0.5f);
+
+    Vector3 depth_normal = controller.transform.TransformDirection(-Vector3.right);
+    float depth_angle = Vector3.Angle(local_top_face, local_bottom_face);
+    Handles.DrawWireArc(origin, depth_normal, local_top_face,
+                        depth_angle, controller.transform.localScale.x * BOX_RADIUS);
   }
 
   public override void OnInspectorGUI() {
