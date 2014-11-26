34bfec61d9abc95d43bfd81b5b935823dee6e23f
Wilbur Yu <yuwilbur@gmail.com>
2014-11-26 14:19:44 -0800

    Added Full Rigid Hand support

---
+++ Assets/LeapMotion+OVR/Scripts/PassthroughEnabler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PassthroughEnabler : MonoBehaviour {
+
+  public GameObject passthroughLeft;
+  public GameObject passthroughRight;
+
+  private HandController controller_;
+
+	// Use this for initialization
+	void Start () {
+    controller_ = GetComponent<HandController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (controller_.GetAllGraphicsHands().Length > 0)
+    {
+      passthroughLeft.SetActive(false);
+      passthroughRight.SetActive(false);
+    }
+    else
+    {
+      passthroughLeft.SetActive(true);
+      passthroughRight.SetActive(true);
+    }
+	}
+}
