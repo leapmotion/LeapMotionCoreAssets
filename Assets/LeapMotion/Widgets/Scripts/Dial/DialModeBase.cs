7738cb6e9ce5ca57a8d3b528f4d0b299e06fbf82
Wilbur Yu <yuwilbur@gmail.com>
2014-12-03 14:45:28 -0800

    Added basic Scroll Text base

--- Assets/LeapMotion/Widgets/Scripts/Dial/DialModeBase.cs
+++ Assets/LeapMotion/Widgets/Scripts/Dial/DialModeBase.cs
@@ -5,19 +5,57 @@ namespace LMWidgets
 {
   public class DialModeBase : MonoBehaviour
   {
+    private GameObject index_ = null;
+    private GameObject thumb_ = null;
+
+    private float angle_pivot_ = 0.0f;
+    private Vector3 direction_pivot_ = Vector3.zero;
+
+    private bool IsHand(Collider other)
+    {
+      return other.transform.parent && other.transform.parent.parent && other.transform.parent.parent.GetComponent<HandModel>();
+    }
+
+    private bool IsFingerTip(Collider other, string finger)
+    {
+      return (other.name == "bone3" && other.transform.parent.name == finger);
+    }
+
     void OnTriggerEnter(Collider other)
     {
-
+      if (IsHand(other))
+      {
+        if (index_ == null || thumb_ == null)
+        {
+          index_ = (index_ == null && IsFingerTip(other, "index")) ? other.gameObject : index_;
+          thumb_ = (thumb_ == null && IsFingerTip(other, "thumb")) ? other.gameObject : thumb_;
+          if (index_ != null && thumb_ != null)
+          {
+            direction_pivot_ = index_.transform.position - thumb_.transform.position;
+          }
+        }
+      }
     }
 
-    void Start()
+    void OnTriggerExit(Collider other)
     {
+      if (other.gameObject == index_)
+      {
+        index_ = null;
+      }
 
+      if (other.gameObject == thumb_)
+      {
+        thumb_ = null;
+      }
     }
 
-    void Update()
+    void FixedUpdate()
     {
-
+      if (index_ != null && thumb_ != null)
+      {
+        
+      }
     }
   }
 }
---
+++ Assets/LeapMotion/Widgets/Scripts/Scroll/ScrollTextBase.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LMWidgets
+{
+  public class ScrollTextBase : ButtonBase
+  {
+    private Vector3 this_pivot_ = Vector3.zero;
+    private Vector3 target_pivot_ = Vector3.zero;
+
+    private GameObject target_ = null;
+
+    private bool IsHand(Collision other)
+    {
+      return other.transform.parent && other.transform.parent.parent && other.transform.parent.parent.GetComponent<HandModel>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+      if (target_ == null && IsHand(other))
+      {
+        target_ = other.gameObject;
+      }
+    }
+
+    private void UpdatePosition(Vector3 displacement)
+    {
+      Vector3 local_position = transform.localPosition;
+      local_position.x = 0.0f;
+      local_position.y = transform.InverseTransformPoint(this_pivot_).y + transform.InverseTransformDirection(displacement).y;
+      local_position.z = Mathf.Max(local_position.z, 0.0f);
+      transform.localPosition = local_position;
+    }
+
+    public override void ButtonPressed()
+    {
+      if (target_ != null)
+      {
+        this_pivot_ = transform.position;
+        target_pivot_ = target_.transform.position;
+      }
+    }
+
+    public override void ButtonReleased()
+    {
+      target_ = null;
+      transform.localPosition = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+      base.Update();
+      if (is_pressed_)
+      {
+        if (target_ != null)
+        {
+          UpdatePosition(target_.transform.position - target_pivot_);
+        }
+      }
+    }
+  }
+}
+
