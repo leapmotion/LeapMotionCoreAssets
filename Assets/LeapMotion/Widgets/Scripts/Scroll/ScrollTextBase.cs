62f72dc1527cbecdc3c413b9f0bd350e95f0f7d4
Wilbur Yu <yuwilbur@gmail.com>
2014-12-03 17:10:57 -0800

    Functional version of scroller

--- Assets/LeapMotion/Widgets/Scripts/Scroll/ScrollTextBase.cs
+++ Assets/LeapMotion/Widgets/Scripts/Scroll/ScrollTextBase.cs
@@ -5,8 +5,11 @@ namespace LMWidgets
 {
   public class ScrollTextBase : ButtonBase
   {
-    private Vector3 this_pivot_ = Vector3.zero;
+    public GameObject content;
+
+    private Vector3 local_pivot_ = Vector3.zero;
     private Vector3 target_pivot_ = Vector3.zero;
+    private Vector3 content_pivot_ = Vector3.zero;
 
     private GameObject target_ = null;
 
@@ -25,19 +28,26 @@ namespace LMWidgets
 
     private void UpdatePosition(Vector3 displacement)
     {
+      Vector3 local_displacement = transform.InverseTransformDirection(displacement);
       Vector3 local_position = transform.localPosition;
       local_position.x = 0.0f;
-      local_position.y = transform.InverseTransformPoint(this_pivot_).y + transform.InverseTransformDirection(displacement).y;
+      local_position.y = local_pivot_.y + local_displacement.y;
       local_position.z = Mathf.Max(local_position.z, 0.0f);
       transform.localPosition = local_position;
+
+      Vector3 content_displacement = content.transform.InverseTransformDirection(displacement);
+      Vector3 content_position = content.transform.localPosition;
+      content_position.y = content_pivot_.y + content_displacement.y * 1000;
+      content.transform.localPosition = content_position;
     }
 
     public override void ButtonPressed()
     {
       if (target_ != null)
       {
-        this_pivot_ = transform.position;
+        local_pivot_ = transform.localPosition;
         target_pivot_ = target_.transform.position;
+        content_pivot_ = content.transform.localPosition;
       }
     }
 
@@ -47,7 +57,6 @@ namespace LMWidgets
       transform.localPosition = Vector3.zero;
     }
 
-    // Update is called once per frame
     public override void Update()
     {
       base.Update();
