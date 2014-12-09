f9200357f9ab8b9928716d1d748eecf526f5581c
Wilbur Yu <yuwilbur@gmail.com>
2014-12-09 15:13:51 -0800

    Functional momentum for the new scrollers

--- Assets/LeapMotion/Widgets/Scripts/Scroll/ScrollTextBase.cs
+++ Assets/LeapMotion/Widgets/Scripts/Scroll/ScrollTextBase.cs
@@ -59,10 +59,7 @@ namespace LMWidgets
     {
       target_ = null;
       transform.localPosition = Vector3.zero;
-      content.rigidbody2D.velocity = new Vector2(
-        content.transform.localPosition.x - prev_content_pos_.x,
-        content.transform.localPosition.y - prev_content_pos_.y
-        );
+      content.rigidbody2D.velocity = (content.transform.localPosition - prev_content_pos_) / Time.deltaTime;
     }
 
     public override void Update()
@@ -75,8 +72,6 @@ namespace LMWidgets
           UpdatePosition(target_.transform.position - target_pivot_);
         }
       }
-      Debug.Log(content.rigidbody2D.velocity.y);
     }
   }
 }
-
