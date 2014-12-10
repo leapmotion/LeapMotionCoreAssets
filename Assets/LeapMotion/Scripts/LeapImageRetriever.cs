d82021b424b2a69782ddd8b13fbc91bde3e5be94
Wilbur Yu <yuwilbur@gmail.com>
2014-12-10 12:44:21 -0800

    Created ScrollTextDemo prefab

--- Assets/LeapMotion/Scripts/LeapImageRetriever.cs
+++ Assets/LeapMotion/Scripts/LeapImageRetriever.cs
@@ -116,7 +116,7 @@ public class LeapImageRetriever : MonoBehaviour
 
   private LM_DEVICE GetDevice(int width)
   {
-    const bool OVERRIDE_MANTIS = true;
+    const bool OVERRIDE_MANTIS = false;
     if (OVERRIDE_MANTIS)
     {
       return LM_DEVICE.MANTIS;
