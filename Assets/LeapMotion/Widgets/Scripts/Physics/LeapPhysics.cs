27873c86528a11c825cfd7992c53ecba001ffee5
Wilbur Yu <yuwilbur@gmail.com>
2015-01-15 10:29:09 -0800

    Added LeapPhysics script to handle collision states

---
+++ Assets/LeapMotion/Widgets/Scripts/Physics/LeapPhysics.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LMWidgets
+{
+  enum LeapPhysicsState
+  {
+    Interacting,
+    Reflecting
+  }
+
+  [RequireComponent(typeof(Rigidbody))]
+  public class LeapPhysics : MonoBehaviour
+  {
+    protected LeapPhysicsState m_state = LeapPhysicsState.Reflecting;
+    private GameObject m_target;
+
+    private bool IsHand(Collider other)
+    {
+      return other.transform.parent && other.transform.parent.parent && other.transform.parent.parent.GetComponent<HandModel>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+      if (m_target == null && IsHand(other))
+      {
+        m_target = other.gameObject;
+        m_state = LeapPhysicsState.Interacting;
+      }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+      if (other.gameObject == m_target)
+      {
+        m_target = null;
+      }
+    }
+
+    protected virtual void FixedUpdate() 
+    {
+      if (m_target == null && m_state == LeapPhysicsState.Interacting)
+      {
+        m_state = LeapPhysicsState.Reflecting;
+      }
+    }
+  }
+}
---
+++ Assets/LeapMotion/Widgets/Scripts/Physics/PhysicsSpring.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsSpring : MonoBehaviour 
+{
+  public float spring = 1000.0f;
+
+  protected float scaled_spring_;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
