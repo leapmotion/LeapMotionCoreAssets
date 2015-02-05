c887a6b759489fc7c43a49da7feac6158ae057f7
protodeep <fox@brainstormer-project.net>
2015-02-04 16:52:27 -0800

    Converting button to 4.6 graphics

--- Assets/LeapMotion/Widgets/Scripts/DemoScripts/ButtonDemoGraphics.cs
+++ Assets/LeapMotion/Widgets/Scripts/DemoScripts/ButtonDemoGraphics.cs
@@ -1,23 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+
 
 public class ButtonDemoGraphics : MonoBehaviour 
 {
-  public void SetActive(bool status)
-  {
-    Renderer[] renderers = GetComponentsInChildren<Renderer>();
-    foreach (Renderer renderer in renderers)
-    {
-      renderer.enabled = status;
-    }
-  }
-
-  public void SetColor(Color color)
-  {
-    Renderer[] renderers = GetComponentsInChildren<Renderer>();
-    foreach (Renderer renderer in renderers)
-    {
-      renderer.material.color = color;
-    }
-  }
+	public void SetActive(bool status)
+	{
+		Renderer[] renderers = GetComponentsInChildren<Renderer>();
+		Text[] texts = GetComponentsInChildren<Text>();
+		Image[] GUIimages = GetComponentsInChildren<Image>();
+		foreach (Renderer renderer in renderers)
+		{
+			renderer.enabled = status;
+		}
+		foreach(Text text in texts){
+			text.enabled = status;
+		}
+		foreach(Image image in GUIimages){
+			image.enabled = status;
+		}
+		
+	}
+	
+	public void SetColor(Color color)
+	{
+		Renderer[] renderers = GetComponentsInChildren<Renderer>();
+		Text[] texts = GetComponentsInChildren<Text>();
+		Image[] GUIimages = GetComponentsInChildren<Image>();
+		foreach (Renderer renderer in renderers)
+		{
+			renderer.material.color = color;
+		}
+		foreach (Text text in texts){
+			text.color = color;
+		}
+		foreach(Image image in GUIimages){
+			image.color = color;
+		}
+	}
 }
