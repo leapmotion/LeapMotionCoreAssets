35c08e249d0225d0b5f0203aea901e69f3fec7da
Amarcolina <Amarcoli@ucsc.edu>
2015-04-24 10:06:15 -0700

    Removed un-needed define

--- Assets/LeapMotion/Scripts/LeapImageRetriever.cs
+++ Assets/LeapMotion/Scripts/LeapImageRetriever.cs
@@ -1,6 +1,4 @@
-﻿//#define OVERRIDE_MANTIS
-
-/******************************************************************************\
+﻿/******************************************************************************\
 * Copyright (C) Leap Motion, Inc. 2011-2014.                                   *
 * Leap Motion proprietary. Licensed under Apache 2.0                           *
 * Available at http://www.apache.org/licenses/LICENSE-2.0.html                 *
