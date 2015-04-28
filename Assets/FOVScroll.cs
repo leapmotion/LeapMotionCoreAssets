0277153aff8af3ac0a1c9a0882154cac8d4908b2
Amarcolina <Amarcoli@ucsc.edu>
2015-04-28 09:16:05 -0700

    Added the FOV scroller for testing

---
+++ Assets/FOVScroll.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FOVScroll : MonoBehaviour {
+    private Camera _camera;
+
+    void Start() {
+        _camera = GetComponent<Camera>();
+    }
+
+    void Update() {
+        _camera.fieldOfView += Input.mouseScrollDelta.y;
+    }
+}
