1fbe5b04215d5c1917e7258449a90afc641e92e8
Gabriel Hare <ghare@leapmotion.com>
2015-06-15 18:23:34 -0700

    Created key controls to switch between camera alignment and scene rescaling.

---
+++ Assets/LeapMotion+OVR/Scripts/CompensatedRescale.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// Enables rescaling of an object while preventing rescaling of specified child objects
+/// </summary>
+public class CompensatedRescale : MonoBehaviour {
+  [Header("Scale-Invariant Children")]
+  public List<Transform> compensated;
+  [Header("Control Keys")]
+  public KeyCode unlockHold = KeyCode.RightShift;
+  public KeyCode resetScale = KeyCode.R;
+  public KeyCode increaseScale = KeyCode.Equals;
+  public KeyCode decreaseScale = KeyCode.Minus;
+  [Range(0,1)]
+  public float increaseFactor = 0.05f;
+
+  private Vector3 initialScale;
+
+	// Use this for initialization
+	void Start () {
+    initialScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  if (unlockHold != KeyCode.None &&
+      !Input.GetKey (unlockHold)) {
+      return;
+    }
+    if (Input.GetKeyDown (resetScale)) {
+      float multiplier = (
+        (initialScale.x / transform.localScale.x) + 
+        (initialScale.y / transform.localScale.y) +
+        (initialScale.z / transform.localScale.z)
+        )/3f;
+      ApplyRescale(multiplier);
+      return;
+    }
+    if (Input.GetKeyDown (increaseScale)) {
+      ApplyRescale(1f + increaseFactor);
+      return;
+    }
+    if (Input.GetKeyDown (decreaseScale)) {
+      ApplyRescale(1f / (1f + increaseFactor));
+      return;
+    }
+	}
+
+  void ApplyRescale(float multiplier) {
+    transform.localScale *= multiplier;
+    foreach (Transform child in compensated) {
+      child.localScale /= multiplier;
+    }
+  }
+}
---
+++ Assets/LeapMotion/Scripts/Utils/KeyEnableMonoBehaviors.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class KeyEnableMonoBehaviors : MonoBehaviour {
+  public List<MonoBehaviour> targets;
+  [Header("Controls")]
+  public KeyCode unlockHold = KeyCode.RightShift;
+  public KeyCode toggle = KeyCode.T;
+  
+  // Update is called once per frame
+  void Update () {
+    if (unlockHold != KeyCode.None &&
+        !Input.GetKey (unlockHold)) {
+      return;
+    }
+    if (Input.GetKeyDown (toggle)) {
+      foreach (MonoBehaviour target in targets) {
+        target.enabled = !target.enabled;
+      }
+    }
+  }
+}
