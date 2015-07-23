82afdb425a3fc270c8871442688d866b3d2545b3
Gabriel Hare <ghare@leapmotion.com>
2015-07-22 21:57:01 -0700

    Created TEST scene demonstrating application of alignment to other objects, without applying alignment to cameras

---
+++ Assets/LeapMotion+OVR/Scripts/RewindGameObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RewindGameObject : MonoBehaviour {
+  public enum CameraType {
+    left = -1,
+    center = 0,
+    right = 1
+  };
+  public CameraType cameraType;
+  public LeapCameraAlignment cameraAlignment;
+	
+	// this.LateUpdate must be called after cameraAlignment.lateUpdate
+  void LateUpdate () {
+    cameraAlignment.RelativeRewind (transform, (int)cameraType);
+	}
+}
