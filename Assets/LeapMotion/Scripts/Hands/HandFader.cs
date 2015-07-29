bf3e233c9bdf30c6e5817aee035ee1455f8aabf0
Wilbur Yu <yuwilbur@gmail.com>
2015-07-29 13:58:37 -0700

    Make handFader inherit-able

--- Assets/LeapMotion/Scripts/Hands/HandFader.cs
+++ Assets/LeapMotion/Scripts/Hands/HandFader.cs
@@ -4,23 +4,23 @@ using System.Collections;
 
 
 public class HandFader : MonoBehaviour {
-    public float confidenceSmoothing = 10.0f;
-    public AnimationCurve confidenceCurve;
+  public float confidenceSmoothing = 10.0f;
+  public AnimationCurve confidenceCurve;
 
-    private HandModel _handModel;
-    private float _smoothedConfidence = 0.0f;
-    private Renderer _renderer;
+  protected HandModel _handModel;
+  protected float _smoothedConfidence = 0.0f;
+  protected Renderer _renderer;
 
-    void Awake() {
-        _handModel = GetComponent<HandModel>();
-        _renderer = GetComponentInChildren<Renderer>();
-        _renderer.material.SetFloat("_Fade", 0);
-    }
+  protected virtual void Awake() {
+    _handModel = GetComponent<HandModel>();
+    _renderer = GetComponentInChildren<Renderer>();
+    _renderer.material.SetFloat("_Fade", 0);
+  }
 
-	void Update () {
-        _smoothedConfidence += (_handModel.GetLeapHand().Confidence - _smoothedConfidence) / confidenceSmoothing;
-        float fade = confidenceCurve.Evaluate(_smoothedConfidence);
-        _renderer.enabled = fade != 0.0f;
-        _renderer.material.SetFloat("_Fade", confidenceCurve.Evaluate(_smoothedConfidence));
-	}
+  protected virtual void Update() {
+    _smoothedConfidence += (_handModel.GetLeapHand().Confidence - _smoothedConfidence) / confidenceSmoothing;
+    float fade = confidenceCurve.Evaluate(_smoothedConfidence);
+    _renderer.enabled = fade != 0.0f;
+    _renderer.material.SetFloat("_Fade", confidenceCurve.Evaluate(_smoothedConfidence));
+  }
 }
