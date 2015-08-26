34e8137b4b2a42dd1891eb075151e9eccc2a25c4
Gabriel Hare <ghare@leapmotion.com>
2015-08-25 22:29:46 -0700

    Path to recording is displayed. (Because paths are usually long, the font size has been reduced.)

--- Assets/LeapMotion/DemoResources/Scripts/RecordingControls.cs
+++ Assets/LeapMotion/DemoResources/Scripts/RecordingControls.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class RecordingControls : MonoBehaviour {
   [Multiline]
   public string header;
+  private string footer = "";
   public GUIText displayGui;
 
   public KeyCode beginRecordingKey = KeyCode.R;
@@ -19,7 +20,7 @@ public class RecordingControls : MonoBehaviour {
   }
 
   void Update() {
-    displayGui.text = header + "\n";
+    if (displayGui != null) displayGui.text = header + "\n";
 
     switch (_controller.GetLeapRecorder().state) {
       case RecorderState.Recording:
@@ -38,40 +39,43 @@ public class RecordingControls : MonoBehaviour {
         allowBeginPlayback();
         break;
     }
+
+    if (displayGui != null) displayGui.text += footer;
   }
 
   private void allowBeginRecording() {
-    displayGui.text += beginRecordingKey + " - Begin Recording\n";
+    if (displayGui != null) displayGui.text += beginRecordingKey + " - Begin Recording\n";
     if (Input.GetKeyDown(beginRecordingKey)) {
       _controller.ResetRecording();
       _controller.Record();
+      footer = "";
     }
   }
 
   private void allowBeginPlayback() {
-    displayGui.text += beginPlaybackKey + " - Begin Playback\n";
+    if (displayGui != null) displayGui.text += beginPlaybackKey + " - Begin Playback\n";
     if (Input.GetKeyDown(beginPlaybackKey)) {
       _controller.PlayRecording();
     }
   }
 
   private void allowEndRecording() {
-    displayGui.text += endRecordingKey + " - End Recording\n";
+    if (displayGui != null) displayGui.text += endRecordingKey + " - End Recording\n";
     if (Input.GetKeyDown(endRecordingKey)) {
       string savedPath = _controller.FinishAndSaveRecording();
-      Debug.Log("Recording saved to: " + savedPath);
+      footer = "Recording saved to:\n" + savedPath;
     }
   }
 
   private void allowPausePlayback() {
-    displayGui.text += pausePlaybackKey + " - Pause Playback\n";
+    if (displayGui != null) displayGui.text += pausePlaybackKey + " - Pause Playback\n";
     if (Input.GetKeyDown(pausePlaybackKey)) {
       _controller.PauseRecording();
     }
   }
 
   private void allowStopPlayback() {
-    displayGui.text += stopPlaybackKey + " - Stop Playback\n";
+    if (displayGui != null) displayGui.text += stopPlaybackKey + " - Stop Playback\n";
     if (Input.GetKeyDown(stopPlaybackKey)) {
       _controller.StopRecording();
     }
