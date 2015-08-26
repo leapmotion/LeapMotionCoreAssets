e563e25dded126a4a5b23c7925de37351b116a8e
Gabriel Hare <ghare@leapmotion.com>
2015-08-25 21:11:08 -0700

    Hand creation & destruction call-back events. Graphics & Physics call-backs can be distinguished by HandModel object properties, such as name.

---
+++ Assets/LeapMotion+OVR/Scripts/TESTHandEvents.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TESTHandEvents : MonoBehaviour {
+  public HandController handController;
+
+	// Use this for initialization
+	void Start () {
+    if (handController == null) {
+      handController = GameObject.FindObjectOfType<HandController>();
+    }
+    if (handController == null) {
+      enabled = false;
+      return;
+    }
+    handController.onCreateHand += onCreateHand;
+    handController.onDestroyHand += onDestroyHand;
+	}
+	
+  public void onCreateHand(HandModel hand) {
+    Debug.Log("onCreateHand ID = " + hand.LeapID() + " for model " + hand.name);
+  }
+
+  
+  public void onDestroyHand(HandModel hand) {
+    Debug.Log("onDestroyHand ID = " + hand.LeapID() + " for model " + hand.name);
+  }
+}
