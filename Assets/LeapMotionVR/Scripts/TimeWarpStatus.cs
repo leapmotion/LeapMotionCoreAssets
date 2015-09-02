0cd090fe0811f3c77a09a292f0628a008a8437d0
unknown <Gabriel@10430-WIN.leap.corp>
2015-09-02 14:42:11 -0700

    Catch cases where alignment is disabled.

--- Assets/LeapMotionVR/Scripts/TimeWarpStatus.cs
+++ Assets/LeapMotionVR/Scripts/TimeWarpStatus.cs
@@ -2,12 +2,29 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 
-public class TouchCubeStatus : MonoBehaviour {
+public class TimeWarpStatus : MonoBehaviour {
   public LeapCameraAlignment cameraAlignment;
+
+  protected Text textField;
+
+  void Start () {
+    textField = GetComponent<Text> ();
+    if (textField == null) {
+      gameObject.SetActive(false);
+    }
+  }
 	
 	// Update is called once per frame
-	void Update () {
-    Text textField = GetComponent<Text> ();
+  void Update () {
+    if (cameraAlignment == null) {
+      Debug.Log ("TimeWarpStatus requires LeapCameraAlignment reference -> status will be disabled");
+      gameObject.SetActive(false);
+      return;
+    }
+    if (!cameraAlignment.isActiveAndEnabled) {
+      return;
+    }
+
     string statusText = "IMAGE LATENCY: " + (cameraAlignment.imageLatency.value / 1000f).ToString("#00.0") + " ms\n";
     statusText += "LEAP RENDER: " + (cameraAlignment.frameLatency.value / 1000f).ToString ("#00.0") + " ms\n";
     statusText += "REWIND ADJUST: " + (cameraAlignment.rewindAdjust).ToString ("#00.0") + " frames\n";
@@ -17,6 +34,7 @@ public class TouchCubeStatus : MonoBehaviour {
 //      statusText += "TIME WARP: " + (OVRManager.display.latency.timeWarp * 1000f).ToString ("#00.0") + " ms\n";
 //      statusText += "POST PRESENT: " + (OVRManager.display.latency.postPresent * 1000f).ToString ("#00.0") + " ms";
 //    }
+
     textField.text = statusText;
 	}
 }
