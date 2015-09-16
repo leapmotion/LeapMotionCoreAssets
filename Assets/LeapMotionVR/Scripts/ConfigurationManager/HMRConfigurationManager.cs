7831564c2afd872539843165c1853e7aa36d3bcd
Daniel Plemmons <dpx109@gmail.com>
2015-09-16 11:02:27 -0700

    Create configuration manager.

---
+++ Assets/LeapMotionVR/Scripts/ConfigurationManager/HMRConfigurationManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HMRConfigurationManager : MonoBehaviour {
+  public enum HMRConfiguration {
+    VR_WORLD_VR_HANDS = 0,
+    VR_WORLD_AR_HANDS = 1,
+    AR_WORLD_AR_HANDS = 2
+  }
+
+  [SerializeField]
+  private HMRConfiguration _configuration;
+
+  [SerializeField]
+  private LMHeadMountedRigConfiguration[] _headMountedConfigurations;
+
+  public GameObject _backgroundQuad;
+  public HandController _handController;
+  public Camera _leftCamera;
+  public Camera _rightCamera;
+  public Camera _centerCamera;
+  public LeapCameraAlignment _aligner;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  public void validateConfigurationsLabeled() {
+    validateEnoughConfigurations();
+    string[] enumNames = System.Enum.GetNames(typeof(HMRConfiguration));
+
+    for (int i = 0; i < _headMountedConfigurations.Length; i++) {
+      if (_headMountedConfigurations[i].configurationName == enumNames[i]) { continue; }
+      _headMountedConfigurations[i].configurationName = enumNames[i];
+    }
+  }
+
+  private void validateEnoughConfigurations() {
+    string[] enumNames = System.Enum.GetNames(typeof(HMRConfiguration));
+
+    if (enumNames.Length > _headMountedConfigurations.Length) {
+      LMHeadMountedRigConfiguration[] configs = new LMHeadMountedRigConfiguration[enumNames.Length];
+      System.Array.Copy(_headMountedConfigurations, configs, _headMountedConfigurations.Length);
+      _headMountedConfigurations = configs;
+    }
+  }
+}
---
+++ Assets/LeapMotionVR/Scripts/ConfigurationManager/LMHeadMountedRigConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public struct LMHeadMountedRigConfiguration  {
+  [SerializeField]
+  private string _configurationName;
+
+  [SerializeField]
+  private bool _enableBackgroundQuad;
+
+  [SerializeField]
+  private HandModel _leftHandGraphicsModel;
+
+  [SerializeField]
+  private HandModel _rightHandGraphicsModel;
+
+  [SerializeField]
+  private bool _enableLeftAndRightCameras;
+
+  [SerializeField]
+  private bool _enableLeftAndRightImageRetrievers;
+  
+  [SerializeField]
+  private bool _enableCenterCamera;
+
+  [SerializeField]
+  private float _tweenTimewarp;
+
+  [SerializeField]
+  private float _tweenPosition;
+
+  [SerializeField]
+  private float _tweenForward;
+
+  public string configurationName { 
+    get { return _configurationName; }
+    set { _configurationName = value; }
+  }
+  public bool enableBackgroundQuad { get { return _enableBackgroundQuad; } }
+  public HandModel leftHandGraphicsModel { get { return _leftHandGraphicsModel; } }
+  public HandModel rightHandGraphicsModel { get { return _rightHandGraphicsModel; } }
+  public bool enableLeftAndRightCameras { get { return _enableLeftAndRightCameras; } }
+  public bool enableLeftAndRightImageRetrievers { get { return _enableLeftAndRightImageRetrievers; } }
+  public bool enableCenterCamera { get { return _enableCenterCamera; } }
+  public float tweenTimewarp { get { return _tweenTimewarp; } }
+  public float tweenPosition { get { return _tweenPosition; } }
+  public float tweenForward { get { return _tweenForward; } }
+
+  public LMHeadMountedRigConfiguration(
+    string name,
+    bool backgroundQuad, 
+    HandModel leftHandModel, HandModel rightHandModel,
+    bool leftAndRightCameras, bool leftAndRightImageRetrievers, bool centerCamera,
+    float timewarp, float position, float forward) {
+      _configurationName = name;
+      _enableBackgroundQuad = backgroundQuad;
+      _leftHandGraphicsModel = leftHandModel;
+      _rightHandGraphicsModel = rightHandModel;
+      _enableLeftAndRightCameras = leftAndRightCameras;
+      _enableLeftAndRightImageRetrievers = leftAndRightImageRetrievers;
+      _enableCenterCamera = centerCamera;
+      _tweenTimewarp = timewarp;
+      _tweenPosition = position;
+      _tweenForward = forward;
+  }
+}
