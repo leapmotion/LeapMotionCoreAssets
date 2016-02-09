6b9873ea4d25bd9478d715adb8a398ae7a5341c1
Amarcolina <Amarcoli@ucsc.edu>
2016-02-08 17:58:40 -0800

    Added prefab and removed requirecomponenty

--- Assets/LeapMotion/Scripts/Hands/HandTransitionBehavior.cs
+++ Assets/LeapMotion/Scripts/Hands/HandTransitionBehavior.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 namespace Leap {
-  [RequireComponent(typeof(HandModel))]
   public abstract class HandTransitionBehavior : MonoBehaviour {
 
     public abstract void Reset();
