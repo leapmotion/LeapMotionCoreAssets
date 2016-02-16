76ddbc3297cedaaa6c50d4c1e0c3f99e40e15c8b
Wilbur <wyu@leapmotion.com>
2016-02-16 12:59:28 -0800

    Combined VR and PC Visualizer to one single Visualizer

---
+++ Assets/LeapMotion/ScenesResources/Scripts/VisualizerManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class VisualizerManager : MonoBehaviour {
+  public GameObject PCVisualizer = null;
+  public GameObject VRVisualizer = null;
+
+  void Awake()
+  {
+    if (VRDevice.isPresent)
+    {
+      PCVisualizer.gameObject.SetActive(false);
+      VRVisualizer.gameObject.SetActive(true);
+    } else
+    {
+      VRVisualizer.gameObject.SetActive(false);
+      PCVisualizer.gameObject.SetActive(true);
+    }
+  }
+
+  void Start()
+  {
+    Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, false);
+  }
+}
