042300073c3696e2c15d95d039e0b11c324e660d
Wilbur <wyu@leapmotion.com>
2016-02-16 15:11:32 -0800

    Added mode and warning text

--- Assets/LeapMotion/ScenesResources/Scripts/VisualizerManager.cs
+++ Assets/LeapMotion/ScenesResources/Scripts/VisualizerManager.cs
@@ -1,26 +1,28 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEngine.VR;
 
 public class VisualizerManager : MonoBehaviour {
-  public GameObject PCVisualizer = null;
-  public GameObject VRVisualizer = null;
+  public GameObject m_PCVisualizer = null;
+  public GameObject m_VRVisualizer = null;
+  public UnityEngine.UI.Text m_modeText;
+  public UnityEngine.UI.Text m_warningText;
 
   void Awake()
   {
     if (VRDevice.isPresent)
     {
-      PCVisualizer.gameObject.SetActive(false);
-      VRVisualizer.gameObject.SetActive(true);
-    } else
-    {
-      VRVisualizer.gameObject.SetActive(false);
-      PCVisualizer.gameObject.SetActive(true);
+      m_PCVisualizer.gameObject.SetActive(false);
+      m_VRVisualizer.gameObject.SetActive(true);
+      m_modeText.text = "VR Mode";
+      m_warningText.text = "";
     }
-  }
-
-  void Start()
-  {
+    else
+    {
+      m_VRVisualizer.gameObject.SetActive(false);
+      m_PCVisualizer.gameObject.SetActive(true);
+      m_modeText.text = "Desktop Mode";
+      m_warningText.text = "Orion is built for virtual reality and performs best when head-mounted";
+    }
     Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, false);
   }
 }
